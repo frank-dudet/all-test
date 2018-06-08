5eab9d8b83477ab48cd92b83da9d4c46a4ce3e88
dudetao <dudetao@meituan.com>
2018-06-08 16:55:58 +0800

    add mock object

--- src/main/java/com/cn/frank/test/mock/HelloService.java
+++ src/main/java/com/cn/frank/test/mock/HelloService.java
@@ -1,5 +1,7 @@
 package com.cn.frank.test.mock;
 
+import com.cn.frank.test.bean.User;
+
 /**
  * Author: frank_du
  * Time : 2018/6/8 下午4:03
@@ -12,5 +14,9 @@ public class HelloService {
 
     public void show() {
         System.out.println("HelloService.show()");
+    }
+
+    public User helloUser(User user) {
+        return user;
     }
 }
--- src/main/java/com/cn/frank/test/mock/TestMock.java
+++ src/main/java/com/cn/frank/test/mock/TestMock.java
@@ -1,5 +1,6 @@
 package com.cn.frank.test.mock;
 
+import com.cn.frank.test.bean.User;
 import org.junit.Test;
 
 import static org.easymock.EasyMock.*;
@@ -16,11 +17,23 @@ public class TestMock {
 
         expect(obj.sayHello("z3")).andReturn("hello l4");
 
+        User user = new User();
+        user.setUsername("User1");
+        user.setPassword("test");
+
+        expect(obj.helloUser(user)).andReturn(new User("hhh", "test")).times(2);
+
         replay(obj);
 
         String actual = obj.sayHello("z3");
 
+        User result = obj.helloUser(user);
+        User result2 = obj.helloUser(user);
+
         System.out.println(actual);
+
+        System.out.println(result);
+        System.out.println(result2);
 
         verify(obj);
     }
