c6a13362d4f993f12870717f763e900fe7c7752b
dudet <dudet1@126.com>
2019-05-06 11:42:12 +0800

    sss

---
+++ src/main/java/com/cn/frank/test/exception/MyException.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package com.cn.frank.test.exception;
+
+/**
+ * @author detao.ddt
+ * @version $Id: MyException, v0.1 2019年03月15日 下午12:48 detao.ddt Exp $
+ */
+public class MyException {
+}
